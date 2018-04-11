4e5592427165fe57268b6c308a018e7c6e580fb0
kyshel <1069072177@qq.com>
2018-04-11 08:08:30 +0800

    Update string_generator.py

--- python/string_generator.py
+++ python/string_generator.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python
+# Remote_version: https://github.com/kyshel/scripts/blob/master/python/string_generator.py
+# Aim: 4 letters string, find ones that have three same character, eg 'aaab' is ok, 'aabc' is not
+# Date: 170410
+# Contact: kyshel.me
 
 import string	
 import os
 import collections
+
+
+text_file = open("out.txt", "w")
 
 for i in xrange(1,26):
 	for j in xrange(1,26):
@@ -18,12 +25,10 @@ for i in xrange(1,26):
 				max_count=collections.Counter(str_wait).most_common(1)[0][1]
 				if max_count == 3:
 					print str_wait
+					text_file.write(str_wait+'\n')
 
 
 
 
 
-
-				# if j > 1:
-				# 	os._exit(0)
-					
+text_file.close()
